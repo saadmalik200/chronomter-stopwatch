8ac9c1bd9fab6eaa3144aee7b2b36abc8571b7d7
Saad Malik <saad_malik200@hotmail.com>
2022-10-17 23:19:45 +0200

    ex1 final commit

--- src/Chronometer.js
+++ src/Chronometer.js
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 function Chronometer() {
   const [isActive, setIsActive] = useState(false);
   const [isPaused, setIsPaused] = useState(true);
-  const [time, setTime] = useState("000");
+  const [time, setTime] = useState("00");
 
   useEffect(() => {
     let interval = null;
-    // countDown(time);
     if (isActive && isPaused === false) {
       interval = setInterval(() => {
         setTime((time) => +time + 10);
@@ -31,21 +30,28 @@ function Chronometer() {
   };
   const resetHandler = () => {
     setIsActive(false);
-    setTime("000");
+    setTime("00");
   };
 
-  const days = 1000 * 60 * 60 * 24;
-  const hours = 1000 * 60 * 60;
-  const minutes = 1000 * 60;
-  const seconds = 1000;
+  const minutes = Math.floor((time / 60000) % 60);
+  const seconds = Math.floor((time / 1000) % 60);
+  const hundredths = (time / 10) % 100;
+
+  const addZero = (n) => {
+    return n < 10 ? "0" + n : n;
+  };
 
   return (
     <div className="bg-gray-200 flex justify-center items-center h-screen">
       <div>
         <h1 className="text-center text-[2rem] mb-5">React Chrono</h1>
         <div className="bg-white p-5">
-          <div className="bg-blue-200 w-[15rem] h-[5rem] mb-3 flex justify-center items-center text-[2rem]">
-            <p>
+          <div className="bg-blue-200 w-[15rem] h-[5rem] mb-3 flex justify-center items-center text-[2.8rem]">
+            <h1>
+              {addZero(minutes)}:{addZero(seconds)}:{addZero(hundredths)}
+            </h1>
+
+            {/* <p>
               {Math.floor(((time % days) % hours) / minutes).toString()
                 .length === 1
                 ? "0" + Math.floor(((time % days) % hours) / minutes)
@@ -61,12 +67,14 @@ function Chronometer() {
                 : Math.floor((((time % days) % hours) % minutes) / seconds)}
             </p>
             <p>:</p>
+
+            {/* <p>{"0" + time.toString().slice(-3, -1)}</p> */}
             {/* <p>
-              {time.toString().slice(-2).length === 0
-                ? "00" + Math.floor(time.toString().slice(-3, -1))
-                : Math.floor(time.toString().slice(-3, -1))}
+              {" "}
+              {time.toString().slice(-3, -1).length === 1
+                ? "00" + time.toString().slice(-3, -2)
+                : time.toString().slice(-3, -1)}
             </p> */}
-            <p>{time.toString().slice(-3, -1)}</p>
           </div>
           <div className="flex gap-3 justify-center">
             <button onClick={startHandler}>START</button>
